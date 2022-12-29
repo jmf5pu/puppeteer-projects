1d06d2c4603251e1da1362c6b8cea5fdb69c3f82
jmf5pu <59934098+jmf5pu@users.noreply.github.com>
2022-12-29 14:05:33 -0500

    clean up bash script

--- viewbot.sh
+++ viewbot.sh
@@ -1,16 +1,30 @@
-echo "spinning up ${1:-1} process(es)..."
+printf "spinning up ${1:-1} process(es)..."
 
-for ((i = 0; i < ${1:-1}; i++))
+for ((j = 0; j < ${1:-1}; j++))
 do
+    i=$((++i%4 + 2)); 
+    printf "\r"   
+    printf '\b|/-\' | cut -b 1,$i | tr -d '\n'; 
+    printf " spinning up ${1:-1} process(es)..."
     tsc viewer.ts | node viewer.js ${2:-120} ${3:-240} ${4:-y} ${5:-y} &
-    pids[${i}]=$!
+    pids[${j}]=$!
 done
 
-echo "wait for each process in ${pids[*]} to exit..."
+(   
+    while sleep 1; do    
+        i=$((++i%4 + 2)); 
+        printf "\r"   
+        printf '\b|/-\' | cut -b 1,$i | tr -d '\n'; 
+        printf " waiting for each process in ${pids[*]} to exit..."
+    done
+) &
+waiting_pid=$!
 
 for pid in ${pids[*]}; 
 do
     wait $pid
 done
 
-echo "all processes have completed!"
+kill $waiting_pid
+
+printf "\33[2K\rall processes have completed!"
